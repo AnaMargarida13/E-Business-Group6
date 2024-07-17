35420a6f387c2c53bafe7722325f000c5efd638e
AnaMargarida13 <149785844+AnaMargarida13@users.noreply.github.com>
2024-07-17 10:00:10 +0800

    small changes to model file

--- model.R
+++ model.R
@@ -14,12 +14,12 @@ data <- read.csv("customer_purchase_data.csv", stringsAsFactors = TRUE)
 # Structure of the data
 str(data)
 
-# 1. Check for missing values
+# Check for missing values
 missing_values <- sapply(data, function(x) sum(is.na(x)))
 print("Missing values in each column:")
 print(missing_values)
 
-# 2. Check for duplicates
+# Check for duplicates
 duplicates <- data[duplicated(data), ]
 num_duplicates <- nrow(duplicates)
 print(paste("Number of duplicate rows:", num_duplicates))
@@ -45,18 +45,18 @@ for (col in categorical_cols) {
   }
 }
 
-# Step 2: Encoding Categorical Variables
+#Encoding Categorical Variables
 #Convert categorical variables to factors
 #data$Gender <- factor(data$Gender, levels = c(0, 1), labels = c("Male", "Female"))
 #data$ProductCategory <- factor(data$ProductCategory, levels = 0:4, labels = c("Electronics", "Clothing", "Home Goods", "Beauty", "Sports"))
 #data$LoyaltyProgram <- factor(data$LoyaltyProgram, levels = c(0, 1), labels = c("No", "Yes"))
 data$PurchaseStatus <- factor(data$PurchaseStatus, levels = c(0, 1), labels = c("No", "Yes"))
 
-# Step 3: Feature Scaling
+#Feature Scaling
 # Scale numerical features
 data[numerical_cols] <- scale(data[numerical_cols])
 
-# Step 4: Splitting the Dataset
+#Splitting the Dataset
 set.seed(123) # for reproducibility
 split = sample.split(data$PurchaseStatus, SplitRatio = .8)
 data_train <- filter(data, split == TRUE) 
